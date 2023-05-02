565eec86f8ce46835058847c2e045eca77ab19c9
Mykhailo Kuzich <timmkedth@gmail.com>
2023-05-02 21:38:37 +0200

    add request for category page

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useGetCategoryQuery } from '../../redax/categoryApi';
 
 const Category: React.FC = () => {
   const { categoryName } = useParams();
+  const { data, isSuccess } = useGetCategoryQuery(categoryName!);
   return (
     <>
       <div>{categoryName}</div>
+      {isSuccess && data.products.map(i => <div>{i}</div>)}
     </>
   );
 };
